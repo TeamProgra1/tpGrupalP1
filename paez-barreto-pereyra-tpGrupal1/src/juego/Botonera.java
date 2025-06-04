020bc917f1222b49fddef51ebafe63afa0a73c8e
TeamProgra1 <roman.andres.pereyra2020@gmail.com>
2025-06-04 07:34:27 -0300

    now

---
+++ paez-barreto-pereyra-tpGrupal1/src/juego/Botonera.java
@@ -0,0 +1,80 @@
+package juego;
+
+import java.awt.Color;
+import java.awt.Image;
+
+import entorno.Entorno;
+import entorno.Herramientas;
+
+public class Botonera {
+	Image botoneraPRINCIPAL,boton1,boton2,boton3,boton4,borde,fondoBotonera,botonPRINCIPAL,poderHielo,poderBolaFuego;
+	double ancho,alto,x,y,bordeIzq,bordeDer,bordeArriba,bordeAbajo,ancho1,alto1,x1,y1,bordeIzq1,bordeDer1,bordeArriba1,bordeAbajo1;
+	public Botonera() {
+		fondoBotonera = Herramientas.cargarImagen("fondoBOTONERA.jpg");
+		botoneraPRINCIPAL = Herramientas.cargarImagen("fondoBOTONERA 2.0.jpg");
+		botonPRINCIPAL = Herramientas.cargarImagen("botonPRINCIPAL.png");
+		// Boton 1
+		this.x = 703;
+		this.y = 200;
+		this.ancho = 105;
+		this.alto = 45;
+		this.bordeIzq = x - ancho/2;
+		this.bordeDer = x + ancho/2;
+		this.bordeArriba = y - ancho/2;;
+		this.bordeAbajo = y + ancho/2;;
+		// Boton 2
+		this.x1 = 703;
+		this.y1 = 300;
+		this.ancho1 = 105;
+		this.alto1 = 45;
+		this.bordeIzq1 = x1 - ancho1/2;
+		this.bordeDer1 = x1 + ancho1/2;
+		this.bordeArriba1 = y1 - ancho1/2;;
+		this.bordeAbajo1 = y1 + ancho1/2;;
+	}
+	public void dibujar(Personaje personaje,Entorno e,boolean selec,boolean selec2) {
+		e.dibujarImagen(fondoBotonera, 910, 300, 0, 1);
+		e.dibujarImagen(botoneraPRINCIPAL, 705, 300, 0,0.5);
+		e.dibujarImagen(botonPRINCIPAL, 705, 45, 0,0.3);
+		e.cambiarFont("Felix Titling", 15, Color.BLACK, 1);
+		e.escribirTexto("LISTA DE PODERES", 633, 50);
+		if (selec == true) {
+			e.dibujarRectangulo(x,y,ancho,alto,0,Color.DARK_GRAY);
+		}else {
+			e.dibujarRectangulo(x,y,ancho,alto,0,Color.GRAY);
+		}
+		if (selec2 == true) {
+			e.dibujarRectangulo(x1,y1,ancho1,alto1,0,Color.DARK_GRAY);
+		}else {
+			e.dibujarRectangulo(x1,y1,ancho1,alto1,0,Color.GRAY);
+		}
+		// Poder 1:
+		
+		if (selec == true) {
+			e.cambiarFont("Felix Titling", 10, Color.GRAY,1);
+			e.escribirTexto("BOMBA DE FUEGO", 654, 205);
+		}else {
+			e.cambiarFont("Felix Titling", 11, Color.WHITE,1);
+			e.escribirTexto("BOMBA DE FUEGO", 654, 205);
+		}
+		
+		// Poder 2:
+		if (selec2 == true) {
+			e.cambiarFont("Felix Titling", 12, Color.GRAY,1);
+			e.escribirTexto("RAYO", 680, 305);
+		}else {
+			e.cambiarFont("Felix Titling", 14, Color.WHITE,1);
+			e.escribirTexto("RAYO", 680, 305);
+		}
+	
+		// Estadisticas - Jugador
+		e.cambiarFont("Felix Titling", 14, Color.WHITE,1);
+		e.escribirTexto("HP:", 655, 400);
+		e.cambiarFont("Felix Titling", 14, Color.WHITE,1);
+		e.escribirTexto("MANA:", 650, 450);
+		e.cambiarFont("Felix Titling", 14, Color.WHITE,1);
+		e.escribirTexto("PUNTUACIÓN:" , 650, 520);
+		e.cambiarFont("Felix Titling",30,Color.YELLOW,1);
+		e.escribirTexto("" + personaje.kills , 700, 550);
+	}
+}
---
+++ paez-barreto-pereyra-tpGrupal1/src/juego/Enemigo.java
@@ -0,0 +1,49 @@
+package juego;
+
+import java.awt.Image;
+import entorno.Entorno;
+import entorno.Herramientas;
+
+	public class Enemigo {
+		int vida;
+		boolean fueAlcanzado1 = false;
+		boolean fueAlcanzado = false;
+		boolean alcanzoAlMago = false;
+		boolean muerto = false;
+		double x,y;
+		Image murcielago;
+		
+		public Enemigo(double x,double y) {
+			this.vida = 1;
+			this.x=x;
+			this.y=y;
+			this.murcielago = Herramientas.cargarImagen("MURCIELAGO.gif");
+		}
+		
+		public void dibujar(Entorno e) {
+			e.dibujarImagen(this.murcielago, this.x, this.y, 0,0.41);
+		}
+			public void moverHacia(double px, double py) {
+				if (this.murcielago != null) {
+					double dx = px - this.x;
+				    double dy = py - this.y;
+				    double distancia = Math.sqrt(dx * dx + dy * dy);
+				    double desviacion = 0.2;
+				    dx += (Math.random() - 0.5)*desviacion;
+				    dy += (Math.random() - 0.6)*desviacion;
+
+				    if (distancia > 1) {
+				        double moverX = dx / distancia;
+				        double moverY = dy / distancia;
+				        this.x += moverX * Math.random();
+				        this.y += moverY * Math.random();
+				    }else {
+				    	if (murcielago != null && distancia <= 1) {
+				    			this.alcanzoAlMago = true;
+				    		}else {
+				    			this.alcanzoAlMago = false;
+				    }
+				    }
+				}
+		}
+	}
